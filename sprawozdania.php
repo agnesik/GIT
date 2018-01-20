6c92e04bb9b83eb0571166f86f2681d5c068486a
agnesik <32746929+agnesik@users.noreply.github.com>
2018-01-20 14:23:46 +0100

    Add files via upload

--- sprawozdania.php
+++ sprawozdania.php
@@ -1,44 +1,44 @@
 <?php
 session_start();
-$studentId = $_SESSION["id"];
-$przedmiotId = $_GET['p'];
-
 include 'connect.php';
 include "header.php";
 
-$result = mysqli_query($con, "SELECT name FROM subjects WHERE id = $przedmiotId LIMIT 1;");
-$name = mysqli_fetch_assoc($result);
-$subjectName = $name['name'];
+if (isset($_POST['subject_name'])) {
+    $name = trim($_POST['subject_name']);
+    $sql = "INSERT INTO `zadania`(`id`, `name`)  VALUES (NULL , '" . $name . "')";
+    mysqli_query($con, $sql);
+}
 ?>
 
 <body>
 
 <div class="container" align='center'>
-    <h2>Twoje pliki z przedmiotu: <?= $subjectName ?></h2>
+    <h2>Twoje zadania z przedmiotu</h2>
+
+    <div class="form-group text-center">
+        <form action="sprawozdania.php?p=" method="post" enctype="multipart/data">
+            <label for="nazwa">Nazwa zadania</label>
+            <input class="name" name="subject_name" type="text" placeholder="">
+            <button type="submit">Dodaj</button>
+
+    </div>
+
     <div class="table-responsive text-center">
         <table class="table table-dark table-striped table-bordered">
             <thead>
             <tr>
                 <th>File Name</th>
-                <th>File Type</th>
-                <th>Uczeń</th>
-                <th>View</th>
-                <th>Usuń</th>
             </tr>
             </thead>
             <tbody>
 
             <?php
-            $sql="SELECT * FROM uploads LEFT JOIN student ON (uploads.student_id=student.id) WHERE subject_id = $przedmiotId   ORDER BY uploads.id DESC"; //desc - malejąco wg id i pokazuje ostatnie dodane
+            $sql="SELECT * FROM  `zadania` "; //desc - malejąco wg id i pokazuje ostatnie dodane
             $result_set=mysqli_query($con, $sql);
             while($row=mysqli_fetch_array($result_set))
             {
                 echo "<tr>";
-                echo "<td><?php echo " . $row['id'] . " ?>" . $row['file']."</td>";
-                echo "<td>" . $row['type'] . "</td>";
-                echo "<td>" . $row['user']." &nbsp  ". $row['pass'] . "</td>";
-                echo "<td><a href=\"uploads/".$row['file']."\" target=\"_blank\">Zobacz plik</a></td>";
-                echo "<td><a href='delete2.php?did=" . $row['id'] . "'>Usuń przedmiot</a></td>";
+                echo "<td><?php echo " . $row['id'] . " ?>" . $row['name']."</td>";
                 echo "</tr>";
             }
             ?>
